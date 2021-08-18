0c0f57237a18eb5a4e8ee76bc3f00aa26ce7a2ef
Zoya <64033883+inesagh@users.noreply.github.com>
2021-08-18 19:56:17 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from './components/header/Header';
+import Pictures from './components/pictures/Pictures';
+import Services from './components/services/Services';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Login from './components/account/Login';
+import Registration from './components/account/Registration';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Pictures />
+      <About />
+      <Services />
+      <Contact />
+      <Login forId={true} forClass={false} />
     </div>
-  );
+  )
 }
 
 export default App;
